aaecbfff5de3e13523cfd67f7058a5bd50cebd3e
ConKernel <conkernel@outlook.com>
2026-05-31 11:30:59 +0200

    Part01_12.IntegerInput Done

--- mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -5,8 +5,11 @@ public class IntegerInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // write your program here
-
+        System.out.println("Give a number:");
+        int numero = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + numero);
+        
+        
+        
     }
 }
