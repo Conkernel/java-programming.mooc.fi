553bebd26234f354da2d6ca2856c1104b36d4d14
ConKernel <conkernel@outlook.com>
2026-05-31 11:42:39 +0200

    Part01_13.DoubleInput Done

--- mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -5,8 +5,8 @@ public class DoubleInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // write your program here
-
+        System.out.println("Write a value");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You gave the number  " + value);
     }
 }
