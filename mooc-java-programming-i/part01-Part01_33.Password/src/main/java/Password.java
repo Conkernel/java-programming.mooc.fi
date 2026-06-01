9a144b1d911fd80949187afc933dcb9d62dbee9a
ConKernel <conkernel@outlook.com>
2026-06-01 12:34:50 +0200

    Part01_33.Password Done

--- mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
+++ mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
@@ -5,7 +5,16 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here 
+        
+        System.out.println("Password?");
+        // Cambiamos forma de leer la string
+        String word = scan.nextLine();
+        
+        // Cambiamos forma de comparar
+        if (word.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
