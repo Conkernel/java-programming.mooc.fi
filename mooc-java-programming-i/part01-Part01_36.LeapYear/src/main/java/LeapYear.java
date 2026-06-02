173303306ee2e5a8d18dbb58fd17ff6399cd7811
ConKernel <conkernel@outlook.com>
2026-06-02 05:01:19 +0200

    Part01_36.LeapYear Done

--- mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,5 +6,16 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scan.nextLine());
+
+        // LÓGICA: Es bisiesto si es divisible por 4 Y (NO es divisible por 100 O SÍ es divisible por 400)
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+        
+        scan.close(); // Buena práctica: cerrar el scanner
     }
 }
